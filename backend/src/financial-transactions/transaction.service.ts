7e687c6f8443dab3dada80433af322e81c4c1dd3
alessandro <alessandroadm@live.com>
2020-11-13 01:39:49 -0300

    feat: add transaction.service

---
+++ backend/src/financial-transactions/transaction.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Transaction, TransactionDocument } from './transaction.schema';
+
+@Injectable()
+export class  TrasnsactionService {
+ constructor(@InjectModel(Transaction.name) private transactionModel: Model<TransactionDocument>) { }
+ 
+ async findAllTransactions(dto: any): Promise<Transaction[]>{
+  const transactions = this.transactionModel.find({});
+  return transactions;
+ }
+
+}
