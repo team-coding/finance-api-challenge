3e7b99dd80a83937bc33a10fd6bcdabec3ab2835
alessandro <alessandroadm@live.com>
2020-11-13 01:29:22 -0300

    feat: add transaction.module

---
+++ backend/src/financial-transactions/transaction.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Transaction, TransactionSchema } from "./transaction.schema";
+
+@Module({
+ imports: [MongooseModule.forFeature([{name:Transaction.name, schema:TransactionSchema}])],
+ controllers: [],
+ providers:[]
+})
+export class TransactionModule{}
