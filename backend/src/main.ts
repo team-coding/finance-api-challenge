a1bd0af49b270ecca465a955aabe48ec2cda13ad
alessandro <alessandroadm@live.com>
2020-11-14 20:11:35 -0300

    chore: install cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import {json, urlencoded} from 'body-parser'
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.use(json());
   app.use(urlencoded({ extended: true }));
   await app.listen(3000);
